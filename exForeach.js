32be1dc693525ab77cdee5bd79887c68af28c605
Otavio Leopoldino da Silva <1052200@senacminas.edu.br>
2025-11-18 16:48:21 -0300

    Exercicio forEach

--- exForeach.js
+++ exForeach.js
@@ -1,7 +1,12 @@
 
 
 let games = ["Minecraft", "Fortnite", "Roblox"];
-
+// Mostrar cada game com emoji
 games.forEach((game,index) => {
   console.log(`${(index + 1)}.${game} `);
 });
+
+//Resultado
+// 1. Minecraft
+// 2. Fortnite
+// 3. Roblox
