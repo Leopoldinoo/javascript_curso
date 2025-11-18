1040063b441658b2682b4aa4cba9b7a59b17bd0f
Otavio Leopoldino da Silva <1052200@senacminas.edu.br>
2025-11-18 16:12:07 -0300

    Exercicio POP de array

--- exPOP.js
+++ exPOP.js
@@ -1,5 +1,5 @@
 let lanches = ["X-burguer","Pizza","Sushi","Açaí"];
 //  Remove o ÚLTIMO elemento
 let removido = lanches.pop();
-console.log(removido); // Açaí
-console.log(lanches);
+console.log(removido); // "Açaí"
+console.log(lanches); // X-burguer, Pizza, Sushi
