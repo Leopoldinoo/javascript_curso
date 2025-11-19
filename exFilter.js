0d4e16b1b8674be2956a0996fd81f6b08516959e
Leopoldinoo <otavioleopoldino1@gmail.com>
2025-11-19 16:49:29 -0300

    Exercicio usando Filter

---
+++ exFilter.js
@@ -0,0 +1,12 @@
+
+//Filtrar notas acima da média usando Arrow function
+let notas = [4.5, 8.0, 6.5, 9.0, 3.0];
+
+//Versão completa com parênteses
+let aprovados = notas.filter((nota) => {
+  return nota >= 7;
+});
+//Todas notas
+console.log(notas);
+//Aprovados
+console.log(aprovados);
