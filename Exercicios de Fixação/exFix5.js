855d16d51fee1aedee9229fdc5538a6979cf8ddc
Leopoldinoo <otavioleopoldino1@gmail.com>
2025-11-19 15:23:35 -0300

    Exercicio de Adicionar 4 lanches e remover os 2 últimos

--- Exercicios de Fixação/exFix5.js
+++ Exercicios de Fixação/exFix5.js
@@ -8,8 +8,11 @@ for(let i = 1; i <= qtd; i++){
   lanches.push(lanche);
 }
   console.log(lanches);
+
   lanches.pop();
+
   console.log(lanches);
+  
   lanches.pop();
 
   lanches.forEach((lanches,index)=> {
