8d0c67c765e118baab003f23f0f1f865b55f378a
Otavio Leopoldino da Silva <1052200@senacminas.edu.br>
2025-11-19 14:16:02 -0300

    Exercicio Playlist

--- desafio.js
+++ desafio.js
@@ -1,11 +1,13 @@
 let playlist = ["Dua Lipa - Dance The Night", " The Weekend - Blinding Lights"];
 
+//Adicionando duas músicas ao finl da playlist
 playlist.push("Rich Amiri - One Call","Don Toliver - Tore UP");
 
 console.log(playlist);
-
+// Removendo a ultima música da playlist
 let removido = playlist.pop();
-
+console.log(removido);
+// Mostrando na tela os itens
 playlist.forEach((playlist,index) => {
   console.log(`${(index + 1)}.${playlist} `);
 });
